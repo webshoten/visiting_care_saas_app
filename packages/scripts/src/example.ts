9623a8ae16f2e09d5370bd789145ec6d875b35ff
webshoten <weboshimashoten@gmail.com>
2025-08-02 21:33:59 +0900

    name fix

--- packages/scripts/src/example.ts
+++ packages/scripts/src/example.ts
@@ -1,4 +1,4 @@
 import { Resource } from "sst";
-import { Example } from "@monorepo-template/core/example";
+import { Example } from "@visiting_app/core/example";
 
 console.log(`${Example.hello()} Linked to ${Resource.MyBucket.name}.`);
--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "visiting_app",
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
