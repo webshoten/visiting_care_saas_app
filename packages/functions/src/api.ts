c33a3ea1831c482f56d5c7c1a87df4582deb1a3b
webshoten <weboshimashoten@gmail.com>
2025-08-03 10:08:59 +0900

    base

--- packages/functions/src/api.ts
+++ packages/functions/src/api.ts
@@ -1,10 +1,21 @@
-import { Resource } from "sst";
+// packages/functions/src/api.ts
+
 import { Handler } from "aws-lambda";
 import { Example } from "@visiting_app/core/example";
+import { User } from "@visiting_app/core/user";
+import { docClient } from "@visiting_app/core/dynamo-db";
 
 export const handler: Handler = async (_event) => {
+  console.log(_event);
+  // DynamoDBのテーブル内容(userId=1)をそのまま返すだけ
   return {
     statusCode: 200,
-    body: `${Example.hello()} Linked to ${Resource.MyBucket.name}.`,
+    body: `${Example.hello()} : ${
+      JSON.stringify(
+        await User.getUser(docClient, {
+          userId: "1",
+        }),
+      )
+    }.`,
   };
 };
--- sst.config.ts
+++ sst.config.ts
@@ -1,20 +1,43 @@
+//sst.config.ts
+
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
   app(input) {
     return {
-      name: "visiting_app",
+      name: "nextjsapp",
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
     };
   },
   async run() {
-    const storage = await import("./infra/storage");
-    await import("./infra/api");
+    const table = new sst.aws.Dynamo("MyTable", {
+      fields: {
+        userId: "string",
+        noteId: "string",
+      },
+      primaryIndex: { hashKey: "userId", rangeKey: "noteId" },
+    });
+
+    const api = new sst.aws.ApiGatewayV2("MyApi", {
+      cors: {
+        allowMethods: ["GET"],
+        allowOrigins: ["*"],
+      },
+      link: [table],
+    });
+
+    api.route("GET /", "packages/functions/src/api.handler");
+
+    new sst.aws.Nextjs("MyWeb", {
+      path: "packages/web",
+      link: [api],
+    });
 
     return {
-      MyBucket: storage.bucket.name,
+      MyApi: api,
+      MyTable: table,
     };
   },
 });
